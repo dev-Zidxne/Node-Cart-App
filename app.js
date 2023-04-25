2b40cd3e0c06764c0c88ff4e851ba6234969df98
Zidane Innis <zidaneinnis@gmail.com>
2023-04-25 18:11:30 -0500

    Fetching data from the database using promises

--- app.js
+++ app.js
@@ -15,9 +15,7 @@ app.set("views", "views");
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
-db.execute("SELECT * FROM products");
-
-db.app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/admin", adminRoutes);
